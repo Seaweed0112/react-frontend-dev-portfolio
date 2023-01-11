76f3a104db586c089099ef4ea8b010abbe4a7ee5
Cedric Zheng <veryhappy13579@gmail.com>
2023-01-10 22:03:31 -0800

    Updates

---
+++ precache-manifest.18dc3e338e7551ab86b2737d99e8aaa9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7f6cd7ee255c7dac2e8b0e7084a4f25",
+    "url": "/react-frontend-dev-portfolio/index.html"
+  },
+  {
+    "revision": "1c5caf9fef6f2cb37889",
+    "url": "/react-frontend-dev-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "eb8279c7bb920d502145",
+    "url": "/react-frontend-dev-portfolio/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "1c5caf9fef6f2cb37889",
+    "url": "/react-frontend-dev-portfolio/static/js/2.3550746a.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/react-frontend-dev-portfolio/static/js/2.3550746a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb8279c7bb920d502145",
+    "url": "/react-frontend-dev-portfolio/static/js/main.ccaa71f7.chunk.js"
+  },
+  {
+    "revision": "8b91e79b160a90e30d4f",
+    "url": "/react-frontend-dev-portfolio/static/js/runtime-main.0af024f6.js"
+  }
+]);
