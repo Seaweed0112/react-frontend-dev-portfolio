a4789d8dca3b7c742e346ac0084a4dba46f27ce1
Cedric Zheng <veryhappy13579@gmail.com>
2023-01-10 22:04:27 -0800

    Updates

---
+++ precache-manifest.9a090eb3bcaea7fda249c4fa1cd7aad1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5c33fb504ed1f5c82b0fb59db84ceb5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1c5caf9fef6f2cb37889",
+    "url": "./static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "13da50bebb87666981af",
+    "url": "./static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "1c5caf9fef6f2cb37889",
+    "url": "./static/js/2.3550746a.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "./static/js/2.3550746a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13da50bebb87666981af",
+    "url": "./static/js/main.df0d520c.chunk.js"
+  },
+  {
+    "revision": "1d6896b13d1316b5acb7",
+    "url": "./static/js/runtime-main.2b4c0fc3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-frontend-dev-portfolio/precache-manifest.18dc3e338e7551ab86b2737d99e8aaa9.js"
+  "./precache-manifest.9a090eb3bcaea7fda249c4fa1cd7aad1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-frontend-dev-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
