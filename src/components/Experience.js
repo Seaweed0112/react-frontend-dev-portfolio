0903dbe5bfdc7557bebf9e079a3f17339c96e427
Cedric Zheng <veryhappy13579@gmail.com>
2023-02-10 20:01:24 -0800

    add detail for experience, add photos

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -5,15 +5,31 @@ import {
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 import Badge from "react-bootstrap/Badge";
+import ExpDetailsModal from "./ExperienceDetailsModal";
+import { Icon } from '@iconify/react';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import myIcon from "images/test.png"
 
 class Experience extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      deps: {},
+      detailsModalShow: false,
+    };
+  }
+
   render() {
+    let detailsModalShow = (data) => {
+      this.setState({ detailsModalShow: true, deps: data });
+    };
+    let detailsModalClose = () => this.setState({ detailsModalShow: false });
     if (this.props.resumeExperience && this.props.resumeBasicInfo) {
       var sectionName = this.props.resumeBasicInfo.section_name.experience;
       var work = this.props.resumeExperience.map(function (work, i) {
         const technologies = work.technologies;
         const mainTechnologies = work.mainTech;
-
+        var image = work.image;
         var mainTech = mainTechnologies.map((technology, i) => {
           return (
             <Badge pill className="main-badge mr-2 mb-2" key={i}>
@@ -37,26 +53,30 @@ class Experience extends Component {
               color: "#fff",
               textAlign: "center",
             }}
-            icon={<i className="fab fa-python experience-icon"></i>}
+            // icon={<i className="fab fa-docker experience-icon"></i>}
+            // icon={<FontAwesomeIcon icon="fas fa-code" />}
+            // icon={<iconify-icon icon="ic:round-code-off"></iconify-icon>}
+            icon={<img src="images/test.png" alt="myicon" />}
             key={i}
           >
-            <div style={{ textAlign: "left", marginBottom: "4px" }}>
-              {mainTech}
+            <div onClick={() => detailsModalShow(work)}>
+              <div style={{ textAlign: "left", marginBottom: "4px" }}>
+                {mainTech}
+              </div>
+              <h3
+                className="vertical-timeline-element-title"
+                style={{ textAlign: "left" }}
+              >
+                {work.title}
+              </h3>
+              <h4
+                className="vertical-timeline-element-subtitle"
+                style={{ textAlign: "left" }}
+              >
+                {work.company}
+              </h4>
+              <div style={{ textAlign: "left", marginTop: "15px" }}>{tech}</div>
             </div>
-
-            <h3
-              className="vertical-timeline-element-title"
-              style={{ textAlign: "left" }}
-            >
-              {work.title}
-            </h3>
-            <h4
-              className="vertical-timeline-element-subtitle"
-              style={{ textAlign: "left" }}
-            >
-              {work.company}
-            </h4>
-            <div style={{ textAlign: "left", marginTop: "15px" }}>{tech}</div>
           </VerticalTimelineElement>
         );
       });
@@ -76,6 +96,11 @@ class Experience extends Component {
         <div className="col-md-8 mx-auto">
           <VerticalTimeline>
             {work}
+            <ExpDetailsModal
+              show={this.state.detailsModalShow}
+              onHide={detailsModalClose}
+              data={this.state.deps}
+            />
             <VerticalTimelineElement
               iconStyle={{
                 background: "#AE944F",
@@ -87,6 +112,7 @@ class Experience extends Component {
               }
             />
           </VerticalTimeline>
+
         </div>
       </section>
     );
