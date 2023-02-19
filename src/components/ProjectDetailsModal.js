172b162c24d632e4dd45a15221bc79d121fa006a
Cedric Zheng <veryhappy13579@gmail.com>
2023-02-19 00:39:37 -0800

    make font bigger

--- src/components/ProjectDetailsModal.js
+++ src/components/ProjectDetailsModal.js
@@ -77,7 +77,7 @@ class ProjectDetailsModal extends Component {
             </AwesomeSlider>
           </div>
           <div className="col-md-10 mx-auto">
-            <h3 style={{ padding: "5px 5px 0 5px" }}>
+            <h2 style={{ padding: "5px 5px 0 5px" }}>
               {title}
               {url ? (
                 <a
@@ -92,7 +92,7 @@ class ProjectDetailsModal extends Component {
                   ></i>
                 </a>
               ) : null}
-            </h3>
+            </h2>
             <p className="modal-description">{description}</p>
             <div className="col-md-12 text-center">
               <ul className="list-inline mx-auto">{tech}</ul>
